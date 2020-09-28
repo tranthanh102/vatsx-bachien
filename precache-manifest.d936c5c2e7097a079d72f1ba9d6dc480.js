1ec11b4679b739d28a34bd16fe2f5f7163e92d10
Chien Phi <Chien.Phi@episerver.com>
2020-09-28 15:10:10 +0700

    Updates

---
+++ precache-manifest.d936c5c2e7097a079d72f1ba9d6dc480.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d85c04558a438c2fe4d869a1864a1ea0",
+    "url": "/vatsx-bachien/index.html"
+  },
+  {
+    "revision": "50e3f433f302652db102",
+    "url": "/vatsx-bachien/static/css/2.f4cffb7b.chunk.css"
+  },
+  {
+    "revision": "c5cba21f67befbe6d902",
+    "url": "/vatsx-bachien/static/css/main.4890ad73.chunk.css"
+  },
+  {
+    "revision": "50e3f433f302652db102",
+    "url": "/vatsx-bachien/static/js/2.c3db871d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/vatsx-bachien/static/js/2.c3db871d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5cba21f67befbe6d902",
+    "url": "/vatsx-bachien/static/js/main.79f5104a.chunk.js"
+  },
+  {
+    "revision": "9135b9e927028e465329",
+    "url": "/vatsx-bachien/static/js/runtime-main.6bf8f06f.js"
+  },
+  {
+    "revision": "b3c927937ea4c3510f06a349d7b11bf8",
+    "url": "/vatsx-bachien/static/media/banner-gif.b3c92793.gif"
+  },
+  {
+    "revision": "b9b58ab1c6f91e2651aab59dcf607598",
+    "url": "/vatsx-bachien/static/media/category-image-1.b9b58ab1.png"
+  },
+  {
+    "revision": "2c5baed940a2a89bda4a45a672cdd0c1",
+    "url": "/vatsx-bachien/static/media/category-image-2.2c5baed9.png"
+  },
+  {
+    "revision": "1b03b84a5c6abeef1f99001ead5f5e99",
+    "url": "/vatsx-bachien/static/media/category-image-3.1b03b84a.png"
+  },
+  {
+    "revision": "e20c37bd7ec843c2612887c0270502e3",
+    "url": "/vatsx-bachien/static/media/category-image-4.e20c37bd.png"
+  },
+  {
+    "revision": "4aaf92d0ed86d1b1165581081977af0d",
+    "url": "/vatsx-bachien/static/media/icon-bars.4aaf92d0.svg"
+  },
+  {
+    "revision": "ca82bc743710e8f84904d035d92bf5cd",
+    "url": "/vatsx-bachien/static/media/icon-caret-down.ca82bc74.svg"
+  },
+  {
+    "revision": "35e219c88cf781f37ca6c619f35e06f2",
+    "url": "/vatsx-bachien/static/media/image-slider-1.35e219c8.png"
+  },
+  {
+    "revision": "b6f29f8ed912562725708dade65be943",
+    "url": "/vatsx-bachien/static/media/image-slider-2.b6f29f8e.png"
+  },
+  {
+    "revision": "01df3c34c8ca86959b94ae89b6d73bbc",
+    "url": "/vatsx-bachien/static/media/image-slider-3.01df3c34.png"
+  },
+  {
+    "revision": "4e70dfda675473c0dcf3d6bb97258ebb",
+    "url": "/vatsx-bachien/static/media/image-slider-4.4e70dfda.png"
+  },
+  {
+    "revision": "c2947ef4915c763e05fb25f8c98e909a",
+    "url": "/vatsx-bachien/static/media/image-slider-5.c2947ef4.png"
+  },
+  {
+    "revision": "0476a2a0024cb8cc7d3f3780d9e26973",
+    "url": "/vatsx-bachien/static/media/product-36.0476a2a0.jpg"
+  },
+  {
+    "revision": "79307297ba4d4dd92d290acb567643bb",
+    "url": "/vatsx-bachien/static/media/product-4.79307297.jpg"
+  },
+  {
+    "revision": "8f3a92270b4f6dffc0b6d9a7165d891f",
+    "url": "/vatsx-bachien/static/media/product-detail-1.8f3a9227.jpg"
+  },
+  {
+    "revision": "4ccbcbffc44cdba412b172c75ec42cc2",
+    "url": "/vatsx-bachien/static/media/size-tutorial.4ccbcbff.png"
+  }
+]);
