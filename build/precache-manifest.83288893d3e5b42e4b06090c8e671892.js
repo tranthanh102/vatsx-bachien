ac8619c21f6aec49ee697fd91161b1a41b7bfa57
Chien Phi <Chien.Phi@episerver.com>
2020-09-30 17:24:52 +0700

    update build

---
+++ build/precache-manifest.83288893d3e5b42e4b06090c8e671892.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d5b94e41b3e58a7626672803365f761",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7469686843471c46434d",
+    "url": "/static/css/2.566c87c4.chunk.css"
+  },
+  {
+    "revision": "e716d7324a6b98ad76ef",
+    "url": "/static/css/main.8486db3a.chunk.css"
+  },
+  {
+    "revision": "7469686843471c46434d",
+    "url": "/static/js/2.c3db871d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c3db871d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e716d7324a6b98ad76ef",
+    "url": "/static/js/main.894ad34e.chunk.js"
+  },
+  {
+    "revision": "37a70207da2a11223830",
+    "url": "/static/js/runtime-main.20818536.js"
+  },
+  {
+    "revision": "b3c927937ea4c3510f06a349d7b11bf8",
+    "url": "/static/media/banner-gif.b3c92793.gif"
+  },
+  {
+    "revision": "b9b58ab1c6f91e2651aab59dcf607598",
+    "url": "/static/media/category-image-1.b9b58ab1.png"
+  },
+  {
+    "revision": "2c5baed940a2a89bda4a45a672cdd0c1",
+    "url": "/static/media/category-image-2.2c5baed9.png"
+  },
+  {
+    "revision": "1b03b84a5c6abeef1f99001ead5f5e99",
+    "url": "/static/media/category-image-3.1b03b84a.png"
+  },
+  {
+    "revision": "e20c37bd7ec843c2612887c0270502e3",
+    "url": "/static/media/category-image-4.e20c37bd.png"
+  },
+  {
+    "revision": "4aaf92d0ed86d1b1165581081977af0d",
+    "url": "/static/media/icon-bars.4aaf92d0.svg"
+  },
+  {
+    "revision": "ca82bc743710e8f84904d035d92bf5cd",
+    "url": "/static/media/icon-caret-down.ca82bc74.svg"
+  },
+  {
+    "revision": "35e219c88cf781f37ca6c619f35e06f2",
+    "url": "/static/media/image-slider-1.35e219c8.png"
+  },
+  {
+    "revision": "b6f29f8ed912562725708dade65be943",
+    "url": "/static/media/image-slider-2.b6f29f8e.png"
+  },
+  {
+    "revision": "01df3c34c8ca86959b94ae89b6d73bbc",
+    "url": "/static/media/image-slider-3.01df3c34.png"
+  },
+  {
+    "revision": "4e70dfda675473c0dcf3d6bb97258ebb",
+    "url": "/static/media/image-slider-4.4e70dfda.png"
+  },
+  {
+    "revision": "c2947ef4915c763e05fb25f8c98e909a",
+    "url": "/static/media/image-slider-5.c2947ef4.png"
+  },
+  {
+    "revision": "0476a2a0024cb8cc7d3f3780d9e26973",
+    "url": "/static/media/product-36.0476a2a0.jpg"
+  },
+  {
+    "revision": "79307297ba4d4dd92d290acb567643bb",
+    "url": "/static/media/product-4.79307297.jpg"
+  },
+  {
+    "revision": "8f3a92270b4f6dffc0b6d9a7165d891f",
+    "url": "/static/media/product-detail-1.8f3a9227.jpg"
+  },
+  {
+    "revision": "4ccbcbffc44cdba412b172c75ec42cc2",
+    "url": "/static/media/size-tutorial.4ccbcbff.png"
+  }
+]);
